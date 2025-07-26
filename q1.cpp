416b27e27a178b8de9dd59536cd5208b34d129ae
adityagupyta <adityaguptaa1105@gmail.com>
2025-07-26 10:55:08 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of integers: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Please enter a positive number.\n");
+        return 1;
+    }
+
+    int arr[n];
+    int sum = 0;
+
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    double average = (double)sum / n;
+    printf("Average: %.2lf\n", average);
+
+    return 0;
+}
